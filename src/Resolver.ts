6d224a24c8d89650698861510a760bf4e2e10967
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2024-04-09 12:26:51 +0200

    initium

---
+++ src/Resolver.ts
@@ -0,0 +1,13 @@
+import { ponder } from "@/generated";
+
+ponder.on("Resolver:AuthorisationChanged", async ({ event, context }) => {
+  console.log(event.args);
+});
+
+ponder.on("Resolver:TextChanged", async ({ event, context }) => {
+  console.log(event.args);
+});
+
+ponder.on("Resolver:PubkeyChanged", async ({ event, context }) => {
+  console.log(event.args);
+});
